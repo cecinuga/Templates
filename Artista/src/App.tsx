41e6b89bf3d526bfeb3a49b35ac07deab2c5e596
Peter Bruegel <masterammo01@gmail.com>
2023-04-24 21:33:05 +0200

    gg

---
+++ Artista/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header, { LinkProps } from './components/Header'
+
+const Home: LinkProps = {label: 'Home', link: "/"}
+const Opere: LinkProps = {label: 'Opere', link:"/opere"}
+const ChiSono: LinkProps = {label: 'Chi Sono', link: "/chi-sono"}
+const Gallerie: LinkProps = {label: 'Gallerie', link: "/gallerie"}
+
+function App() {
+  return (
+    <>
+      <Header mainLinks={[Home, Opere, ChiSono, Gallerie]} />
+    </>
+  )
+}
+
+export default App
---
+++ Artista/src/components/Header.tsx
@@ -0,0 +1,108 @@
+import { useState } from 'react';
+import { createStyles, Header as MantineHeader, Container, Anchor, Group, Burger, rem, Text, Title } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+const HEADER_HEIGHT = rem(84);
+
+const useStyles = createStyles((theme) => ({
+  inner: {
+    height: HEADER_HEIGHT,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  burger: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  links: {
+    paddingTop: theme.spacing.lg,
+    height: HEADER_HEIGHT,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  mainLinks: {
+    marginRight: `calc(${theme.spacing.sm} * -1)`,
+  },
+
+  mainLink: {
+    textTransform: 'uppercase',
+    fontSize: rem(13),
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[6],
+    padding: `${rem(7)} ${theme.spacing.sm}`,
+    fontWeight: 700,
+    borderBottom: `${rem(2)} solid transparent`,
+    transition: 'border-color 100ms ease, color 100ms ease',
+
+    '&:hover': {
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+      textDecoration: 'none',
+    },
+  },
+
+  mainLinkActive: {
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    borderBottomColor: theme.colors.red[theme.colorScheme === 'dark' ? 5 : 6],
+  },
+}));
+
+export interface LinkProps {
+  label: string;
+  link: string;
+}
+
+interface DoubleHeaderProps {
+  mainLinks: LinkProps[];
+}
+
+export default function Header({ mainLinks }: DoubleHeaderProps){
+
+    const [opened, { toggle }] = useDisclosure(false);
+    const { classes, cx } = useStyles();
+    const [active, setActive] = useState(0);
+
+    const mainItems = mainLinks.map((item, index) => (
+        <Anchor<'a'>
+        href={item.link}
+        key={item.label}
+        className={cx(classes.mainLink, { [classes.mainLinkActive]: index === active })}
+        onClick={(event) => {
+            event.preventDefault();
+            setActive(index);
+        }}
+        >
+        {item.label}
+        </Anchor>
+    ));
+
+    return(
+       <>
+        <MantineHeader height={HEADER_HEIGHT} mb={120}>
+            <Container className={classes.inner}>
+                <Title 
+                    fw={500} 
+                    order={2}
+                    variant="gradient"
+                    gradient={{ from: 'red.9', to: 'orange.3', deg: 45 }}
+                >   Big Mama
+                </Title>
+                <div className={classes.links}>
+                    <Group spacing={0} position="right" className={classes.mainLinks}>
+                        {mainItems}
+                    </Group>
+                </div>
+                <Burger opened={opened} onClick={toggle} className={classes.burger} size="sm" />
+            </Container>
+        </MantineHeader>
+       </> 
+    )
+}
