d318f0f69705db190acef02dffdb3dd5cb2a3cb0
Peter Bruegel <masterammo01@gmail.com>
2023-04-24 20:36:47 +0200

    gg

--- Ristorante/src/App.tsx
+++ Ristorante/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Header, { LinkProps } from './components/Header'
+
+const Home: LinkProps = {label: "Home", link:"/"}
+const Menu: LinkProps = {label: "Menu", link:"/menu"}
+const La_Brigata: LinkProps = {label: "La Brigata", link:"/la-brigata"}
+const Prenota: LinkProps = {label: "Prenota", link:"/prenota"}
 
 function App() {
 
   return (
     <>
-      
+      <Header mainLinks={[Home, Menu, La_Brigata, Prenota]} />
     </>
   )
 }
