5309bcde1acbf6eee7d4da1308a241468d6a9074
Peter Bruegel <masterammo01@gmail.com>
2023-05-13 19:01:47 +0200

    gg

--- Artista/src/components/AvivaPage.tsx
+++ Artista/src/components/AvivaPage.tsx
@@ -25,10 +25,11 @@ import img12 from "../public/aviva/random/12.jpg"
 import img13 from "../public/aviva/random/13.jpg"
 import img14 from "../public/aviva/random/14.jpg"
 import img15 from "../public/aviva/random/15.jpg"
-import { Group, Text, useMantineTheme } from "@mantine/core"
+import { Group, Table, Text, useMantineTheme } from "@mantine/core"
 import { Link } from "react-router-dom"
 import { useState } from "react"
 import FotoAScatto from "./FotoAScatto"
+import { useMediaQuery } from "@mantine/hooks"
 
 export default function AvivaPage(){
     const theme = useMantineTheme()
@@ -43,85 +44,92 @@ export default function AvivaPage(){
     const [hoverEtichettaBassa6, setHoverEtichettaBassa6] = useState(false);
     const [hoverEtichettaBassa7, setHoverEtichettaBassa7] = useState(false);
     const [Nob, setNob] = useState(false);
+    const lg = useMediaQuery("(max-width: 1024px)")
+    const md = useMediaQuery("(max-width: 768px)")
+    const sm = useMediaQuery("(max-width: 512px)")
+    const scalemd = useMediaQuery("(max-width: 860px)")
+    const scalesm = useMediaQuery("(max-width: 478px)")
 
     return(
-        <div style={{backgroundColor:"black",position:"absolute", width:"100vw", top:"0"}}>
-            <div style={{position:"relative", width:"100%", height:"100%", top:"5rem"}}>
-                <table align="center" style={{position:"relative", top:"8%", left:"10%"}}>
-                    <tr style={{padding:"0"}}>
+        <div style={{backgroundColor:"black",position:"absolute", width:"100%", top:"0"}}>
+            <div style={{position:"relative", width:"100%", height:"100%", top:"0rem"}}>
+                <table width={sm?"100%":md?"450px":lg?"512px":"1024px"} align="center" style={{scale:scalesm?"0.3":scalemd?"0.5":"1",display:"block",position:"relative"}}>
+                    <tr style={{width:md?"50%":"100%",display:"block",padding:"0"}}>
                         <td style={{padding:"0"}} height={83} colSpan={7}>
                             <img src={wrappersopra} style={{position:"relative",zIndex:20, top:"0.1rem"}} />
                         </td>
                     </tr>
-                    <tr style={{padding:"0"}}>
+                    <tr style={{width:md?"50%":"100%",display:"block",padding:"0"}}>
                         <td style={{padding:"0"}} colSpan={4}>
                             <img src={wrappermezzo} />
                         </td>
                     </tr>
-                    <tr style={{padding:"0"}}>
+                    <tr style={{width:md?"50%":"100%",display:"block",padding:"0"}}>
                         <td style={{padding:"0"}} colSpan={7}>
-                            <img src={index_05} width={700} height={19} style={{position:"relative", bottom:"0.4rem"}}></img>
+                            <img src={index_05} width={700} height={19} style={{position:"relative", bottom:"0.5rem"}}></img>
                         </td>
                     </tr>
-                    <tr>
+                    <tr style={{width:md?"50%":"100%",display:"block"}}>
                         <td style={{padding:"0", position:"relative", }} colSpan={3}>
-                            <img src={index_06} style={{position:"relative", bottom:"0.6rem"}}  width={261} height={32}/>
+                            <img src={index_06} style={{position:"relative", bottom:"9.rem"}}  width={261} height={32}/>
                         </td>
-                        <td colSpan={2} rowSpan={10} style={{position:"relative", top:"6.6rem",right:"27.8rem"}}>
-                            <FotoAScatto timeout={2500} foto={[img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12,img13, img14, img15]} />
+                        <td colSpan={2} rowSpan={10} style={{position:"relative", top:"15.5em",left:"0rem"}}>
+                            <div style={{position:"relative", width:"20rem", height:"20rem"}}>
+                                <FotoAScatto timeout={2500} foto={[img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12,img13, img14, img15]} />
+                            </div>
                         </td>
                     </tr>
-                    <tr>
+                    <tr style={{width:md?"50%":"100%",display:"block", position:"relative",bottom:"18.7rem"}}>
                         <td width={155} height={18} style={{paddingLeft:"2rem", position:"relative", bottom:"0.8rem"}}>
                             <Link to="/aviva/corporate-profile" style={{textDecoration:"none"}}><Text onPointerEnter={()=>setHoverEtichetta1(true)} onPointerLeave={()=>setHoverEtichetta1(false)} w={180} c="white" bg={hoverEtichetta1?theme.colors.red[8]:theme.colors.gray[9]} fz={14} align="center" display={"inline-block"} px="1rem"><strong>CORPORATE PROFILE</strong></Text></Link>
                         </td>
                     </tr>
-                    <tr>
+                    <tr style={{width:md?"50%":"100%",display:"block", position:"relative",bottom:"18.7rem"}}>
                         <td style={{paddingLeft:"3rem",}}>
                             <img src={index_11} style={{position:"relative",left:"9.7rem",bottom:"2.6rem",zIndex:0}} />
                         </td>
                     </tr>
-                    <tr>
+                    <tr style={{width:md?"50%":"100%",display:"block", position:"relative",bottom:"18.7rem"}}>
                         <td width={155} height={18} style={{paddingLeft:"2rem", position:"relative", bottom:"15.2rem"}}>
                             <Link style={{textDecoration:"none"}} to="/aviva/progetti-e-servizi"><Text onPointerEnter={()=>setHoverEtichetta2(true)} onPointerLeave={()=>setHoverEtichetta2(false)} w={180} c="white" bg={hoverEtichetta2?theme.colors.red[8]:theme.colors.gray[9]} fz={14} align="center" display={"inline-block"} px="1rem"><strong>PROGETTI E SERVIZI </strong></Text></Link>
                         </td>
                     </tr>
-                    <tr>
+                    <tr style={{width:md?"50%":"100%",display:"block", position:"relative",bottom:"18.7rem"}}>
                         <td style={{paddingLeft:"3rem",}}>
                             <img src={index_12} style={{position:"relative", bottom:"18.4rem",zIndex:0}} />
                         </td>
                     </tr>
-                    <tr>
+                    <tr style={{width:md?"50%":"100%",display:"block", position:"relative",bottom:"18.7rem"}}>
                         <td width={155} height={18} style={{paddingLeft:"2rem", position:"relative", bottom:"16.2rem"}}>
                             <Link style={{textDecoration:"none"}} to="/aviva/i-nostri-successi"><Text onPointerEnter={()=>setHoverEtichetta3(true)} onPointerLeave={()=>setHoverEtichetta3(false)} w={180} c="white" bg={hoverEtichetta3?theme.colors.red[8]:theme.colors.gray[9]} fz={14} align="center" display={"inline-block"} px="1rem"><strong>I NOSTRI SUCCESSI</strong></Text></Link>
                         </td>
                     </tr>
-                    <tr>
+                    <tr style={{width:md?"50%":"100%",display:"block", position:"relative",bottom:"18.7rem"}}>
                         <td style={{paddingLeft:"3rem",}}>
                             <img src={index_12} style={{position:"relative", bottom:"19.3rem",zIndex:10}} />
                         </td>
                     </tr>
-                    <tr>
+                    <tr style={{width:md?"50%":"100%",display:"block", position:"relative",bottom:"18.7rem"}}>
                         <td width={155} height={18} style={{paddingLeft:"2rem", position:"relative", bottom:"17rem"}}>
                             <Text w={180} c="white" bg={theme.colors.gray[9]} fz={14} align="center" display={"inline-block"} px="5rem" style={{height:"1.5rem"}}></Text>
                         </td>
                     </tr>
-                    <tr>
+                    <tr style={{width:md?"50%":"100%",display:"block", position:"relative",bottom:"18.7rem"}}>
                         <td style={{paddingLeft:"3rem",}}>
                             <img src={index_12} style={{position:"relative", bottom:"20.8rem",zIndex:0}} />
                         </td>
                     </tr>
-                    <tr>
+                    <tr style={{width:md?"50%":"100%",display:"block", position:"relative",bottom:"18.7rem"}}>
                         <td style={{paddingLeft:"3rem",}}>
                             <img src={index_18} style={{position:"relative", bottom:"19.5rem",zIndex:0}} />
                         </td>
                     </tr>
-                    <tr>
+                    <tr style={{width:md?"50%":"100%",display:"block", position:"relative",bottom:"18.7rem"}}>
                         <td>
                             <img src={index_20} style={{position:"relative", bottom:"19.7rem",zIndex:0}} />
                         </td>
                     </tr>
-                    <tr>
+                    <tr style={{width:md?"50%":"100%",display:"block", position:"relative", bottom:"18.7rem"}}>
                         <td style={{position:"relative", bottom:"20rem"}}>
                             <Group position="center" bg={theme.colors.gray[9]} w={"100%"}>
                                 <Text fz={12} onPointerEnter={()=>{setHoverEtichettaBassa1(true)}} onPointerLeave={()=>{setHoverEtichettaBassa1(false)}} c={hoverEtichettaBassa1?theme.colors.cyan[5]:"white"}>ARREDAMENTO</Text>
@@ -134,14 +142,14 @@ export default function AvivaPage(){
                             </Group>
                         </td>
                     </tr>
-                    <tr>
+                    <tr style={{width:md?"50%":"100%",display:"block", position:"relative", bottom:"18.7rem"}}>
                         <td align="center">
                             <img src={relatedlinks} style={{position:"relative", bottom:"17rem",zIndex:0}} />
                         </td>
                     </tr>
-                    <tr>
+                    <tr style={{width:md?"50%":"100%",display:"block", position:"relative", bottom:"18.7rem"}}>
                         <td style={{position:"relative",bottom:"15rem"}} align="center">
-                            <img onPointerEnter={()=>{setNob(true)}} onPointerLeave={()=>{setNob(false)}} src={!Nob?nob1:nob2} style={{paddingRight:"150px"}}/><img src={aavivaa} />
+                            <Link to="/"><img onPointerEnter={()=>{setNob(true)}} onPointerLeave={()=>{setNob(false)}} src={!Nob?nob1:nob2} style={{paddingRight:"150px"}}/><img src={aavivaa} /></Link>
                         </td>
                     </tr>
                 </table>
--- Artista/src/components/AvivaWrapper.tsx
+++ Artista/src/components/AvivaWrapper.tsx
@@ -1,26 +1,28 @@
-//import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { ReactElement } from "react"
 import scheda_02 from "../public/aviva/corporate/scheda_02.jpg"
 import scheda_01 from "../public/aviva/corporate/scheda_01.jpg"
+import { Button, useMantineTheme } from "@mantine/core"
+import { AiOutlineBackward } from "react-icons/ai"
 
 type AvivaWrapperProps = {
     children: ReactElement
 }
 
 export default function AvivaWrapper(props: AvivaWrapperProps){
-    //const navigate = useNavigate()
+    const navigate = useNavigate()
+    const theme = useMantineTheme()
 
     return(
         <div style={{backgroundColor:"black",position:"absolute", width:"100%", top:"0",paddingTop:"7rem", paddingBottom:"18.8rem"}}>
             <div style={{position:"relative", width:"100%"}}>
                 <div style={{width:"fit-content",position:"relative", left:"50%",bottom:"0.2rem", transform:"translateX(-50%)", paddingRight:"45rem"}}><img src={scheda_01}></img></div>
                 <div style={{width:"fit-content",position:"relative", left:"50%",bottom:"26.5rem", transform:"translateX(-50%)", paddingRight:"1.3rem"}}><img src={scheda_02} ></img></div>
-                <div style={{display:"inline-block",position:"absolute", bottom:"25.5%", left:"48%", transform:"translateX(-48%)"}}>
-                    
-                {/*<div onClick={()=>{navigate("/aviva")}} style={{cursor:"pointer",width:"fit-content",position:"relative", left:"420px",top:"12.4rem", transform:"translateX(-50%)", paddingRight:"45rem"}}><img src={scheda_06}></img></div><*/}
-                </div>
                 <div style={{position:"absolute", top:"20%", left:"48%", transform:"translateX(-48%)"}}>
                     {props.children}
+                </div>
+                <div style={{display:"inline-block",position:"relative", left:"27.5%", transform:"translateX(-48%)"}}>
+                    <Button variant="transparent" onClick={()=>navigate(-1)}><AiOutlineBackward color={theme.colors.gray[8]} size={48}/></Button>
                 </div>
             </div>
         </div>
